70af208894de7aff6c0532bbe17ac85e37d7e0df
Angeldevelop <you@example.com>
2021-10-01 20:40:21 -0500

    Ajustes finales

--- public/gen-show-icons.php
+++ public/gen-show-icons.php
@@ -5,8 +5,12 @@ function gen_show_icons(){
     $query = "SELECT * FROM {$wpdb->prefix}gen_icons";
     $icons_list = $wpdb->get_results($query, ARRAY_A);
 
-    if(empty($icons_list)){
+    $queryGen = "SELECT * FROM {$wpdb->prefix}gen_icons_general";
+    $data_gen = $wpdb->get_results($queryGen, ARRAY_A);
+
+    if(empty($icons_list) && empty($data_gen)){
         $icons_list = null;
+        $data_gen = null;
     } else {
         ?>
         <div class="gen-float-icons">
@@ -24,23 +28,29 @@ function gen_show_icons(){
                     
                     ?>
                     <li data-css='<?php echo $json_data_css; ?>' class="gen-float-icons__list__item gen-float-icons__item-data">
-                        <span class="gen-float-icons__list__label"><?php echo $item['title']; ?></span>
                         <a 
                             class="gen-float-icons__list__link gen-link <?php echo $item['style'] ?>" 
                             href="<?php echo $item['link']; ?>"
                             target="_blank">
                             <i class="<?php echo $item['faIcon']; ?>"></i>
                         </a>
+                        <span class="gen-float-icons__list__label" style="text-align: <?php echo $item['alignLabelText']; ?>"><?php echo $item['title']; ?></span>
                     </li>
                 <?php endforeach; ?>
+
+                <?php foreach($data_gen as $item): ?>
                     <li class="gen-float-icons__list__item">
-                        <span class="gen-float-icons__list__label">
-                            ¡Hablemos!
-                        </span>
+                        
                         <a class="gen-float-icons__list__link gen-link-toggle" href="#">
                             <i class="fas fa-comment-dots"></i> 
                         </a>
+                        <?php if($item['textLabelClose']): ?>
+                        <span class="gen-float-icons__list__label" style="text-align:<?php echo $item['alignLabelTextGen']; ?>">
+                            <?php echo $item['textLabelClose']; ?>
+                        </span>
+                        <?php endif; ?>
                     </li>
+                <?php endforeach; ?>
             </ul>
         </div>
         <?php
